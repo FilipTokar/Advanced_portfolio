33440b99b6631a4b82db5498e1f396623e74e9d2
Filip <filip.tokar@seznam.cz>
2023-04-11 21:29:05 +0200

    finishing modal UI and adding contanct script

---
+++ script.js
@@ -0,0 +1,25 @@
+// 
+// 
+// 
+
+function contact(event) {
+    event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_dr0tlvs',
+            'template_s4mg16u',
+            event.target,
+            'kDawTHB-YO75r8W1q'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily disabled. Please contact me directly at: filip.tokar@seznam.cz"
+            )
+        })
+}
